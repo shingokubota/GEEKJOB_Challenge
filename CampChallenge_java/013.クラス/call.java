190ed77b6ee1f44907b8094a4ce6a3d1535f299e
LAPTOP-VJHEH5NJ\Kubota <luffa.shingo.cylindrica@gmail.com>
2018-06-22 14:54:33 +0900

    クラス　クラスの作成を提出します

---
+++ CampChallenge_java/013.クラス/call.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Kubota
+ */
+public class call {
+    public static void main(String[] args) {
+        sakusei a=new sakusei();
+        
+        a.name="くぼた";
+        a.number=26;
+        
+        a.deta("くぼた",26);
+        a.deta2("くぼた",26);
+    }
+}
